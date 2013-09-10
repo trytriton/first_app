d7caeb519f31d7d5c784e8dc3e9095f1dd4c7dfb
Michael Richardson <michael@baobablogic.com>
2013-09-10 18:35:33 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FistApp::Application.config.secret_key_base = 'e855cc3413694c68168a54eee48e61dc08639b18f0a8c60518df17b1bc3ff27e752f2154ec91f45fd442810fb359f5ec0e1eafc09b332c67a4d4da80862ecaac'
